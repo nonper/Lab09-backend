0386ebf6f91e64f4b50767d0d15a3a063f69e296
supachok jrirarojkul <supachok8728@gmail.com>
2022-09-15 21:17:21 +0700

    5.8

--- src/main/java/se331/rest/controller/EventController.java
+++ src/main/java/se331/rest/controller/EventController.java
@@ -1,13 +1,12 @@
 package se331.rest.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import se331.rest.entity.Event;
 import se331.rest.service.EventService;
@@ -25,17 +24,13 @@ public class EventController {
     @GetMapping("event")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false) Integer perPage
             , @RequestParam(value = "_page", required = false) Integer page) {
-        List<Event> output = null;
+        Page<Event> pageOutput = eventService.getEvents(perPage, page);
         HttpHeaders responseHeader = new HttpHeaders();
-        responseHeader.set("x-total-count",String.valueOf(eventService.getEventSize()));
+        responseHeader.set("x-total-count",
+                String.valueOf(pageOutput.getTotalElements()));
 
-        try {
-            output = eventService.getEvents(perPage,page);
-            return new ResponseEntity<>(output,responseHeader,HttpStatus.OK);
-        } catch (IndexOutOfBoundsException ex) {
-            return new ResponseEntity<>(output,responseHeader,HttpStatus.OK);
-        }
-
+        return new
+                ResponseEntity<>(pageOutput.getContent(), responseHeader,HttpStatus.OK);
     }
 
     @GetMapping("event/{id}")
